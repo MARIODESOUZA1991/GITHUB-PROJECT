5025c569819f34bb7f72803f399cb3541936a452
m2-desouza <m2-desouza@STU-CZC7268TPH>
2023-03-02 20:17:22 +0000

    Add project files.

---
+++ GITHUB PROJECT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GITHUB_PROJECT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
