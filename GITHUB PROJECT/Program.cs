a5329cddec04bf917e468946d38440752d3e0276
MARIODESOUZA1991 <MARIODESOUZA1991@GMAIL.COM>
2023-03-02 20:40:32 +0000

    I did the menu

--- GITHUB PROJECT/Program.cs
+++ GITHUB PROJECT/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-------MENU-------");
+            Console.WriteLine("WELCOME TO THE SPICE RESTAURANT");
+            Console.WriteLine("Please Select your option");
+            Console.WriteLine("Type 1 for Indian food or Type 2 for British food ");
+            Console.ReadLine();
         }
     }
 }
